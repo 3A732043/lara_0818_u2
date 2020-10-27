3ec48dd779aba0e7db6e049682174aac08581749
Wu Chen-En <ap9941002@gmail.com>
2020-10-27 11:00:59 +0800

    練習2-2:編輯 index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,5 +7,13 @@
 <body>
     <h1>Home</h1>
     <p>This is my body content.</p>
+
+    @extends('layouts.master')
+    @section('title', 'Home')
+    @section('content')
+        <h1>Home</h1>
+        <p>This is my body content.</p>
+    @endsection
+
 </body>
 </html>

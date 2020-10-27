fdd3c259cd54baa24abdd3658daa0cb950681130
Wu Chen-En <ap9941002@gmail.com>
2020-10-27 11:13:29 +0800

    練習2-3:編輯master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,8 +3,15 @@
 <title>@yield('title') | My Blog</title>
 </head>
 <body>
+
 <div class="container">
     @yield('content')
+
+    @include('layouts.partials.navigation')
+    <div class="container">
+        @yield('content')
+    </div>
+
 </div>
 </body>
 </html>
